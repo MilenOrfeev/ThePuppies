5894377926f9146c9545e31f3170803ee01b2ce4
heldt.pawel <2268686h@student.gla.ac.uk>
2019-06-27 11:48:42 +0100

    hoveringNotWork

--- client/client.py
+++ client/client.py
@@ -1,45 +1,45 @@
-import tkinter as tk
+from tkinter import *
 from PIL import ImageTk, Image
+import re
+from HoverInfo import HoverInfo
 
-class Example(tk.Frame):
-    def __init__(self, *args, **kwargs):
-        tk.Frame.__init__(self, *args, **kwargs)
-        self.l1 = tk.Label(self, text="Hover over me")
-        self.l2 = tk.Label(self, text="", width=40)
-        self.l1.pack(side="top")
-        self.l2.pack(side="top", fill="x")
+class Circle(Frame):
+    def __init__(self, parent=None):
+        Frame.__init__(self, parent)
+        self.grid()
+        self.lbl = Label(self, text='testing')
+        self.lbl.grid()
+        self.hover = HoverInfo(self, 'kjsdfhkjsadh')
+        circle1 = canvas.create_oval(100,100,90,90)
+        canvas.tag_bind(circle1,'<ButtonPress-1>',onObjectClick)
 
-        self.l1.bind("<Enter>", self.on_enter)
-        self.l1.bind("<Leave>", self.on_leave)
 
-    def on_enter(self, event):
-        self.l2.configure(text="Hello world")
 
-    def on_leave(self, enter):
-        self.l2.configure(text="")
-
+def onObjectClick(event):
+    print('Got object click', event.x, event.y)
+    print(event.widget.find_closest(event.x, event.y))
 
 #This creates the main window of an application
-window = tk.Tk()
+window = Tk()
 
+#Creates a Tkinter-compatible photo image, which can be used everywhere Tkinter expects an image object.
 path = "office_layout.png"
 img = ImageTk.PhotoImage(Image.open(path))
 
 
-canvas = tk.Canvas(window,width=640,height=449,bg="white")
+
+canvas = Canvas(window,width=640,height=449,bg="white")
 canvas.grid()
 map = canvas.create_image(640, 200, image=img, anchor="e")
 canvas.tag_lower(map)
 
-circle1 = canvas.create_oval(100,100,90,90)
+circle = Circle()
 
-#Creates a Tkinter-compatible photo image, which can be used everywhere Tkinter expects an image object.
 
-#The Label widget is a standard Tkinter widget used to display a text or image on the screen.
 
-#The Pack geometry manager packs widgets in rows or columns.
+
+
 
 
 #Start the GUI
-# Example(window).pack(side="top", fill="both", expand="true")
 window.mainloop()
