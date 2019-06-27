44c9221e99b4a3de2e6be82da03e2a26c458cd99
Dom <domrest@ymail.com>
2019-06-27 14:17:23 +0100

    review menu

--- client/reviewMenu.py
+++ client/reviewMenu.py
@@ -2,7 +2,18 @@ from tkinter import *
 
 
 class ReviewMenu(Menu):
+
+    def callback(self):
+        print("hello")
+
+
     def __init__(self):
         window = Tk()
-        canvas = Canvas(window,width=300,height=400,bg="white")
-        canvas.grid()
+
+        frame = Frame(window, width= 200, height = 400)
+        frame.grid(row = 1)
+        a = Button(window, text = "edit")
+
+        a.grid(row = 0)
+
+        mainloop()
