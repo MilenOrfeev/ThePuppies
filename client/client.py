685b036d0b0b93247cbf68ce7db4954ed05842d0
heldt.pawel <2268686h@student.gla.ac.uk>
2019-06-27 13:54:13 +0100

    2nd menu

--- client/client.py
+++ client/client.py
@@ -3,52 +3,43 @@ from PIL import ImageTk, Image
 from HoverInfo import HoverInfo
 from reviewMenu import ReviewMenu
 
-class Example(Frame):
-
-    def __init__(self, *args, **kwargs):
-        Frame.__init__(self, *args, **kwargs)
-        self.l1 = Label(self, text="Hover over me")
-        self.l2 = Label(self, text="", width=40)
-        self.l1.pack(side="top")
-        self.l2.pack(side="top", fill="x")
 
 class Circle(Frame):
     def draw_circle(self, center_x, center_y, radius):
         return self.create_oval(center_x - radius, center_y - radius, center_x + radius, center_y + radius, activefill="grey")
     Canvas.draw_circle = draw_circle
 
+
     def __init__(self, sid, parent=None):
         Frame.__init__(self, parent)
-        self.grid()
+
         self.sid = sid
-        self.lbl = Label(self, text='testing')
-        self.lbl.grid()
         self.hover = HoverInfo(self, 'kjsdfhkjsadh')
+
         circle1 = canvas.draw_circle(100, 120, 10)
-        canvas.tag_bind(circle1,'<ButtonPress-1>',onObjectClick(sid))
+        canvas.tag_bind(circle1,'<ButtonPress-1>',onObjectClick)
 
 
-
-def onObjectClick(sid):
-    ReviewMenu(sid)
+def onObjectClick(event):
+    print(event.widget.find_closest(event.x, event.y))
+    ReviewMenu()
 
 
 #This creates the main window of an application
 window = Tk()
+canvas = Canvas(window,width=640,height=449,bg="white")
+canvas.grid()
 
 #Creates a Tkinter-compatible photo image, which can be used everywhere Tkinter expects an image object.
 path = "office_layout.png"
 img = ImageTk.PhotoImage(Image.open(path))
-
-
-
-canvas = Canvas(window,width=640,height=449,bg="white")
-canvas.grid()
 map = canvas.create_image(640, 200, image=img, anchor="e")
 canvas.tag_lower(map)
 
+circle = Circle("a3")
+print('a3', circle)
 
-circle = Circle("A11111")
+
 
 
 
--- client/reviewMenu.py
+++ client/reviewMenu.py
@@ -2,8 +2,7 @@ from tkinter import *
 
 
 class ReviewMenu(Menu):
-    def __init__(self,sid):
+    def __init__(self):
         window = Tk()
         canvas = Canvas(window,width=300,height=400,bg="white")
         canvas.grid()
-        print(sid)
