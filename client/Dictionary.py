b8c200e8143d3bfec22df8782d27656b4f6972b2
Darrel Uchendu <2236676u@student.gla.ac.uk>
2019-06-27 15:03:52 +0100

    corrected circle positions

--- client/Dictionary.py
+++ client/Dictionary.py
@@ -1,8 +1,8 @@
-employeeDictionary = { '0001': {'SID': 'A11111', 'Name': 'Maisie McGregor', 'Role': 'Long Term Intern', 'Team': 'Everest', 'Coordinates': [200,200]},
-                       '0002': {'SID': 'B11111', 'Name': 'Domenico Restuccia', 'Role': 'Long Term Intern','Team': 'CPS', 'Coordinates': [300,200]},
-                       '0003': {'SID': 'C11111', 'Name': 'Milen Orfeev', 'Role': 'Long Term Intern', 'Team': 'Mobile Services', 'Coordinates': [350,400]},
-                       '0004': {'SID': 'D11111', 'Name': 'Samuel Kacer', 'Role': 'Long Term Intern', 'Team': 'Abacus Team', 'Coordinates': [100,200]},
-                       '0005': {'SID': 'E11111', 'Name': 'Darrel Uchendu', 'Role': 'Long Term Intern', 'Team': 'On A Team', 'Coordinates': [500,290]},
-                       '0006': {'SID': 'F11111', 'Name': 'Anda Andronie', 'Role': 'Long Term Intern', 'Team': 'On A Team', 'Coordinates': [400,300]},
-                       '0007': {'SID': 'G11111', 'Name': 'Pawel Heldt', 'Role': 'Long Term Intern', 'Team': 'On A Team', 'Coordinates': [100,260]}
+employeeDictionary = { '0001': {'SID': 'A11111', 'Name': 'Maisie McGregor', 'Role': 'Long Term Intern', 'Team': 'Everest', 'Coordinates': [190,170]},
+                       '0002': {'SID': 'B11111', 'Name': 'Domenico Restuccia', 'Role': 'Long Term Intern','Team': 'CPS', 'Coordinates': [480,210]},
+                       '0003': {'SID': 'C11111', 'Name': 'Milen Orfeev', 'Role': 'Long Term Intern', 'Team': 'Mobile Services', 'Coordinates': [400,110]},
+                       '0004': {'SID': 'D11111', 'Name': 'Samuel Kacer', 'Role': 'Long Term Intern', 'Team': 'Abacus Team', 'Coordinates': [300,110]},
+                       '0005': {'SID': 'E11111', 'Name': 'Darrel Uchendu', 'Role': 'Long Term Intern', 'Team': 'On A Team', 'Coordinates': [530,60]},
+                       '0006': {'SID': 'F11111', 'Name': 'Anda Andronie', 'Role': 'Long Term Intern', 'Team': 'On A Team', 'Coordinates': [370,360]},
+                       '0007': {'SID': 'G11111', 'Name': 'Pawel Heldt', 'Role': 'Long Term Intern', 'Team': 'On A Team', 'Coordinates': [100,360]}
                        }
