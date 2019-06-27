c6c023ecf374b9e27b1d6a72a45c7e333eb652d5
Darrel Uchendu <2236676u@student.gla.ac.uk>
2019-06-27 16:19:06 +0100

    remove unused dictionary

--- Dictionary.py
+++
@@ -1,8 +0,0 @@
-employeeDictionary = { '2': {'SID': 'A11111', 'Name': 'Maisie McGregor', 'Role': 'Long Term Intern', 'Team': 'Everest', 'Coordinates': [200,200]},
-                       '3': {'SID': 'B11111', 'Name': 'Domenico Restuccia', 'Role': 'Long Term Intern','Team': 'CPS', 'Coordinates': [300,200]},
-                       '4': {'SID': 'C11111', 'Name': 'Milen Orfeev', 'Role': 'Long Term Intern', 'Team': 'Mobile Services', 'Coordinates': [350,400]},
-                       '5': {'SID': 'D11111', 'Name': 'Samuel Kacer', 'Role': 'Long Term Intern', 'Team': 'Abacus Team', 'Coordinates': [100,200]},
-                       '6': {'SID': 'E11111', 'Name': 'Darrel Uchendu', 'Role': 'Long Term Intern', 'Team': 'On A Team', 'Coordinates': [500,290]},
-                       '7': {'SID': 'F11111', 'Name': 'Anda Andronie', 'Role': 'Long Term Intern', 'Team': 'On A Team', 'Coordinates': [400,300]},
-                       '8': {'SID': 'G11111', 'Name': 'Pawel Heldt', 'Role': 'Long Term Intern', 'Team': 'On A Team', 'Coordinates': [100,260]}
-                       }
