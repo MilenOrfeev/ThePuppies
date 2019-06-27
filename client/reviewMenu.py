6028c8dab31d8ee61f874a32ae9077173ef78a31
maisie_mcgregor@outlook.com <srb17158@uni.strath.ac.uk>
2019-06-27 16:59:47 +0100

    functionality to remove an employee from a chair

--- client/reviewMenu.py
+++ client/reviewMenu.py
@@ -19,24 +19,13 @@ class EditMenu(Menu):
 
     def __init__(self):
         window = Tk()
-        window.title("Edit Employee Information")
 
-        window.configure();
-        a = Label(window, text="First Name").grid(row=0, column=0)
-        b = Label(window, text="Last Name").grid(row=1, column=0)
-        c = Label(window, text="Seat ID").grid(row=2, column=0)
-        d = Label(window, text="Job Role").grid(row=3, column=0)
-        a1 = Entry(window).grid(row=0, column=1)
-        b1 = Entry(window).grid(row=1, column=1)
-        c1 = Entry(window).grid(row=2, column=1)
-        d1 = Entry(window).grid(row=3, column=1)
-
-        def clicked():
-            res = "Welcome to " + txt.get()
-            lbl.configure(text=res)
-
-        btn = Button(window, text="Submit").grid(row=4, column=0)
-        window.mainloop()
+        # frame = Frame(window, width=400, height=400, bg = 'red')
+        # frame.grid(row=0)
+        l1 = Label(window,
+                   text="employee removed from chair")
+        l1.pack(anchor='center')
+        mainloop()
 
 class ReviewMenu(Menu):
 
@@ -65,7 +54,7 @@ class ReviewMenu(Menu):
         def callback2():
             ReportMenu(self.Name)
             # form to edit
-        EDIT = Button(window, text = "edit employee", command=callback)
+        EDIT = Button(window, text = "Remove "+ str(self.Name) +" from Chair", command=callback)
         REPORT = Button(window, bg = 'red', fg = 'red', text="REPORT!!!!!", command=callback2)
         EDIT.grid(row = 1, column = 0)
         REPORT.grid(row = 1, column = 1)
